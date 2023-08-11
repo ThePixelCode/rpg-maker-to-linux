0024354793b5fa074e0ab66eb7b3e2da901e82bb
ThePixelCode <72231627+thepixelcode@users.noreply.github.com>
2023-08-10 23:57:48 -0500

    Moved NWJS comparation method from lib to config

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,7 @@
+use regex::Regex;
 use serde::{Deserialize, Serialize};
+
+use crate::{errors::Errors, NWJS_URL};
 
 #[derive(Debug, Clone, Default, PartialEq, Deserialize, Serialize)]
 pub struct FileAsociation {
@@ -17,20 +20,80 @@ pub struct FileAsociation {
 //     }
 // }
 
-#[derive(Debug, Clone, Default, PartialEq, Deserialize, Serialize)]
+#[derive(Debug, Clone, Default, Deserialize, Serialize)]
 pub struct NWJS {
     pub nwjs_version: String,
     pub especific_nwjs_commands: Vec<String>,
 }
 
-// impl NWJS {
-//     pub fn new(nwjs_version: String, especific_nwjs_commands: Vec<String>) -> Self {
-//         Self {
-//             nwjs_version,
-//             especific_nwjs_commands,
-//         }
-//     }
-// }
+impl PartialEq for NWJS {
+    fn eq(&self, other: &Self) -> bool {
+        self.nwjs_version == other.nwjs_version
+    }
+}
+
+impl Eq for NWJS {}
+
+impl PartialOrd for NWJS {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let components1: Vec<&str> = self.nwjs_version.split(".").collect();
+        let components2: Vec<&str> = other.nwjs_version.split(".").collect();
+        for (comp1, comp2) in components1.iter().zip(components2.iter()) {
+            let num1: u32 = comp1.parse().unwrap_or(0);
+            let num2: u32 = comp2.parse().unwrap_or(0);
+
+            match num1.cmp(&num2) {
+                std::cmp::Ordering::Equal => continue,
+                ord => return Some(ord),
+            }
+        }
+
+        Some(components1.len().cmp(&components2.len()))
+    }
+}
+
+impl Ord for NWJS {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let components1: Vec<&str> = self.nwjs_version.split(".").collect();
+        let components2: Vec<&str> = other.nwjs_version.split(".").collect();
+        for (comp1, comp2) in components1.iter().zip(components2.iter()) {
+            let num1: u32 = comp1.parse().unwrap_or(0);
+            let num2: u32 = comp2.parse().unwrap_or(0);
+
+            match num1.cmp(&num2) {
+                std::cmp::Ordering::Equal => continue,
+                ord => return ord,
+            }
+        }
+
+        components1.len().cmp(&components2.len())
+    }
+}
+
+impl NWJS {
+    pub fn get_version(&self) -> Result<String, Errors> {
+        let regex = Regex::new(&format!(
+            "v{}/",
+            self.nwjs_version.replace(".", "\\.").replace("*", "\\d")
+        ))?;
+
+        let response = reqwest::blocking::get(NWJS_URL)?;
+        let body = response.text()?;
+
+        let mut versions: Vec<String> = regex
+            .captures_iter(&body)
+            .map(|capture| String::from(&capture[0]).replace("/", ""))
+            .collect();
+
+        versions.pop().ok_or(Errors::Unknown)
+    }
+    // pub fn new(nwjs_version: String, especific_nwjs_commands: Vec<String>) -> Self {
+    //     Self {
+    //         nwjs_version,
+    //         especific_nwjs_commands,
+    //     }
+    // }
+}
 
 #[derive(Debug, Clone, Default, PartialEq, Deserialize, Serialize)]
 pub struct Config {
--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,11 @@ mod config;
 mod errors;
 pub mod processor;
 
+const NWJS_URL: &str = "https://dl.nwjs.io";
+const NWJS_NORMAL_URL_FORMAT: &str = "{url}/{version}/nwjs-{version}-linux-x64.tar.gz";
+const NWJS_SDK_URL_FORMAT: &str = "{url}/{version}/nwjs-sdk-{version}-linux-x64.tar.gz";
+
 pub fn print_error_and_gracefully_exit(error: Errors) -> ! {
     println!("Error happened: {}", error);
     exit(1);
-}
-
-pub fn compare_nsjw_versions(ver1: &str, ver2: &str) -> std::cmp::Ordering {
-    let components1: Vec<&str> = ver1.split(".").collect();
-    let components2: Vec<&str> = ver2.split(".").collect();
-    for (comp1, comp2) in components1.iter().zip(components2.iter()) {
-        let num1: u32 = comp1.parse().unwrap_or(0);
-        let num2: u32 = comp2.parse().unwrap_or(0);
-
-        match num1.cmp(&num2) {
-            std::cmp::Ordering::Equal => continue,
-            ord => return ord,
-        }
-    }
-
-    components1.len().cmp(&components2.len())
 }
