a75116b37e383eab86174e2d6600f3e39920a60e
ThePixelCode <thepixelcode@proton.me>
2023-12-27 17:48:24 +0000

    Parse args from steam

--- src/lib.rs
+++ src/lib.rs
@@ -45,3 +45,59 @@ pub fn get_cache_folder() -> Result<std::path::PathBuf, GeneralErrors> {
     }
     Ok(path.to_path_buf())
 }
+
+pub fn parse_steam_args(args: Vec<String>) -> Vec<String> {
+    let mut new_args = Vec::new();
+    let mut program = false;
+    let mut wanted = true;
+    let mut arg_index = 3_i8;
+
+    for arg in args {
+        // FIXME: Make a more general solution
+        if arg_index == 0 {
+            new_args.push(String::from("--"));
+        }
+        if arg_index >= 0 {
+            arg_index -= 1;
+        }
+        if arg != "--" && !program {
+            if wanted {
+                new_args.push(arg);
+            }
+            continue;
+        }
+        if arg == "--" {
+            program = true;
+            if wanted {
+                new_args.push(arg);
+            }
+            wanted = true;
+            continue;
+        }
+        if arg.contains("Steam/ubuntu12_32") {
+            new_args.push(arg);
+            program = false;
+            continue;
+        }
+        if arg.contains("common/SteamLinuxRuntime") {
+            program = false;
+            wanted = false;
+            continue;
+        }
+        if arg.contains("common/Proton") {
+            wanted = false;
+            continue;
+        }
+        if arg == "waitforexitandrun" {
+            program = false;
+            wanted = true;
+            continue;
+        }
+        new_args.push(arg);
+    }
+    let last = new_args.pop().unwrap();
+    let mut last = last.split("/").collect::<Vec<&str>>();
+    let _ = last.pop();
+    new_args.push(last.join("/"));
+    new_args
+}
