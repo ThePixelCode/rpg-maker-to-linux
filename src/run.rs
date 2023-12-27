ef57ff0edc23fcc0629d9281e02e128e2446b6c3
ThePixelCode <thepixelcode@proton.me>
2023-12-27 18:20:56 +0000

    Added run for steam

--- src/run.rs
+++ src/run.rs
@@ -8,6 +8,7 @@ pub enum RunErrors {
 
 type Result<T> = std::result::Result<T, RunErrors>;
 
+// FIXME: Join run_game and run_steam_game into a single function
 pub fn run_game<P>(
     game_data: &crate::GameData<P>,
     logger: &mut crate::logger::Logger,
@@ -21,6 +22,26 @@ where
         .args(args)
         .current_dir(game_data.path.as_ref().display().to_string())
         .spawn()?;
+    logger.log("process created waiting for exit...");
+    let exit_status = run.wait()?;
+    if !exit_status.success() {
+        return Err(RunErrors::RunError(exit_status.code().unwrap_or(128)));
+    }
+    Ok(())
+}
+
+pub fn run_steam_game(
+    mut program_and_args: Vec<String>,
+    logger: &mut crate::logger::Logger,
+) -> Result<()> {
+    let program = program_and_args.remove(0);
+    let last = program_and_args.last_mut().unwrap();
+    last.push_str("/nw");
+    let args = program_and_args;
+
+    logger.log("creating proccess...");
+    let mut run = std::process::Command::new(program).args(args).spawn()?;
+
     logger.log("process created waiting for exit...");
     let exit_status = run.wait()?;
     if !exit_status.success() {
